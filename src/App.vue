940a24a1c0ad0267c8d7606c8e8b049314b789b3
Archie Ogneva <ognevaarchie@gmail.com>
2022-01-27 17:06:34 +0300

    test

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="content"> 
+    <UserProfile />
+    <BeerProfile />
+  </div>
+</template>
+
+<script>
+import UserProfile from './components/UserProfile.vue'
+import BeerProfile from './components/BeerProfile.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    UserProfile,
+    BeerProfile
+  }
+}
+</script>
+
+<style>
+  .content {
+display:flex;
+  }
+
+@media  screen and (max-width: 768px) {
+
+.content {
+flex-direction: column; 
+ }
+}
+</style>
